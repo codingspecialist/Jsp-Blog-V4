4431d114110e94935a7de567fcfa10ec5a1bf706
ssar <ssar@nate.com>
2020-06-09 16:47:08 +0900

    회원정보 수정시 비밀번호 해쉬 업데이트 완료

--- src/com/cos/blog/action/user/UsersUpdateProcAction.java
+++ src/com/cos/blog/action/user/UsersUpdateProcAction.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 import com.cos.blog.action.Action;
 import com.cos.blog.model.Users;
 import com.cos.blog.repository.UsersRepository;
+import com.cos.blog.util.SHA256;
 import com.cos.blog.util.Script;
 
 public class UsersUpdateProcAction implements Action{
@@ -38,7 +39,8 @@ public class UsersUpdateProcAction implements Action{
 		}
 		
 		int id = Integer.parseInt(request.getParameter("id"));
-		String password = request.getParameter("password");
+		String rawPassword = request.getParameter("password");
+		String password = SHA256.encodeSha256(rawPassword);
 		String email = request.getParameter("email");
 		String address = request.getParameter("address");
 		
